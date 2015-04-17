0af013c68305b0eb279156774b749fe417a764fb
Ashley Washington <aw@aecurated.com>
2015-04-17 01:41:15 -0400

    Added ryu-cool
    Does not work.

--- js/app.js
+++ js/app.js
@@ -22,8 +22,24 @@ $(document).ready(function (){
 	.mouseup(function(){
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
-	});
+	})
 });
+
+ 
+$(document).keydown(function(event) {
+		if (event.which == 88) {
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-throwing').hide();
+			$('.ryu-cool').show();
+		}
+	})
+	.keyup(function(event) {
+		if (event.which == 88) {
+			$('.ryu-cool').hide();
+			$('.ryu-still').show();
+		}
+	});
 
 function playHadouken () {
 	$('#hadouken-sound')[0].volume = 0.5;
